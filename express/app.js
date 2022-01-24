a6a798fc493b65f1e629248930ff917d45810f8a
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-01-24 21:45:30 +0900

    2022-01-24

--- express/app.js
+++ express/app.js
@@ -4,14 +4,17 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 dotenv.config();
-
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
@@ -24,11 +27,55 @@ app.use(session({
     },
     name: 'session-cookie',
 }));
+app.use(bodyParser.raw());
+app.use(bodyParser.text());
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+const multer = require('multer');
+const fs = require('fs');
+
+try{
+    fs.readdirSync('uploads');
+}catch(error){
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs.mkdirSync('uploads');
+}
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done){
+            done(null, 'uploads/');
+        },
+        filename(req, file, done){
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html/multipart.html'));
+});
+app.post('/upload', 
+    upload.fields([{name : 'image1'}, {name : 'image2'}]),
+    (req, res) => {
+        console.log(req.files, req.body);
+        res.send('ok');
+    }
+);
 
 // app.get('/', (req, res) => {
 //     //res.send('Hello, Express');
 //     res.sendFile(path.join(__dirname, './html/index.html'));
 // });
+
+
 
 app.use((req, res, next) => {
     console.log('모든 요청에 다 실행됩니다.');
