bcbc192c1ef15f6f3632e42d01f4518a3dd4842b
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-01-27 23:17:57 +0900

    2022-01-27

--- mongo/schemas/index.js
+++ mongo/schemas/index.js
@@ -1,22 +1,21 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    if(process.env.NODE_ENV !== 'prodution'){
+    if(process.env.NODE_ENV !== 'production'){
         mongoose.set('debug', true);
     }
+    mongoose.connect('mongodb://root:rnrgml789@localhost:27017/admin', {
+        dbName: 'nodejs',
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    }, (error) => {
+        if(error){
+            console.log('몽고디비 연결 에러', error);
+        }else{
+            console.log('몽고디비 연결 성공');
+        }
+    });
 }
-
-mongoose.connect('mongdb://localhost:27017/admin', {
-    dbName: 'nodejs',
-    useNewUrlParser: true,
-    useCreateIndex: true,
-}, (error) => {
-    if(error){
-        console.log('몽고디비 연결 에러', error);
-    }else{
-        console.log('몽고디비 연결 성공');
-    }
-});
 
 mongoose.connection.on('error', (error) => {
     console.log('몽고디비 연결 에러', error);
