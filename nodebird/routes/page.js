178a839ff09652c697ff5e7ccc988a8505609c33
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-01-29 20:55:48 +0900

    2022-01-29

---
+++ nodebird/routes/page.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { isLoggedin, isNotLoggedin } = require('./middlewares');
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+    res.locals.user = req.user,
+    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
+    res.locals.followerIdList = [];
+    next();
+});
+
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile', { title: '내 정보 - NodeBird' });
+});
+
+router.get('/join', isNotLoggedin, (req, res) => {
+    res.render('join', { title: '회원가입 - NodeBird' });
+});
+
+router.get('/', (req, res, next) => {
+    const twits = [];
+    res.render('main', {
+        title: 'NodeBird',
+        twits,
+    });
+});
+
+module.exports = router;
