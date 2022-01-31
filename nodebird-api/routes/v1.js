63a00b7a34442df15604eca3e56a36e2e1605bbe
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-01-31 20:46:55 +0900

    2022-01-31

--- nodebird-api/routes/v1.js
+++ nodebird-api/routes/v1.js
@@ -8,15 +8,16 @@ const router = express.Router();
 
 router.post('/token', async (req, res) => {
     const { clientSecret } = req.body;
-    try{
+    try {
         const domain = await Domain.findOne({
             where: { clientSecret },
             include: {
                 model: User,
-                attributes: ['nick', 'id'],
+                attribute: ['nick', 'id'],
             },
         });
         if(!domain){
+            console.log(domain);
             return res.status(401).json({
                 code: 401,
                 message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요.',
@@ -26,12 +27,12 @@ router.post('/token', async (req, res) => {
             id: domain.User.id,
             nick: domain.User.nick,
         }, process.env.JWT_SECRET, {
-            exprireIn: '1m',
-            issuer: 'nodebord',
+            expiresIn: '1m',
+            issuer: 'nodebird',
         });
         return res.json({
             code: 200,
-            messasge: '토큰이 발급되었습니다.',
+            message: '토큰이 발급되었습니다.',
             token,
         });
     }catch(error){
