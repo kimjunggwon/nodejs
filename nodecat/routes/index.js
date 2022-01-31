bc3691ff76c6753d065058ae4f2bc9e9864865e5
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-01-31 23:22:01 +0900

    2022-01-31

--- nodecat/routes/index.js
+++ nodecat/routes/index.js
@@ -2,8 +2,60 @@ const express = require('express');
 const axios   = require('axios');
 
 const router = express.Router();
+const URL = 'http://localhost:8002/v2';
 
-router.get('/test', async (req, res, next) => {
+axios.defaults.headers.origin = 'http://localhost:4000';
+const request = async (req, api) => {
+    try{
+        if(!req.session.jwt){
+            const tokenResult = await axios.post(`${URL}/token`, {
+                clientSecret: process.env.CLIENT_SECRET,
+            });
+            req.session.jwt = tokenResult.data.token;
+        }
+        return await axios.get(`${URL}${api}`, {
+            headers: {
+                authorization: req.session.jwt
+            },
+        });
+    }catch(error){
+        if(error.response.status === 419){
+            delete req.session.jwt;
+            return request(req, api);
+        }
+        return error.response;
+    }
+};
+
+router.get('/mypost', async (req, res, next) => {
+    try {
+        const result = await request(req, '/posts/my');
+        res.json(result.data);
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/search/:hashtag', async (req, res, next) => {
+    try {
+        const result = await request(
+            req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`,
+        );
+        res.json(result.data);
+    }catch(error){
+        if(error.code){
+            console.error(error);
+            next(error);
+        }
+    }
+});
+
+router.get('/', (req, res) => {
+    res.render('main', { key: process.env.CLIENT_SECRET });
+});
+
+/*router.get('/test', async (req, res, next) => {
     try{
         if(!req.session.jwt){
             const tokenResult = await axios.post('http://localhost:8002/v1/token', {
@@ -28,6 +80,6 @@ router.get('/test', async (req, res, next) => {
         }
         return next(error);
     }
-});
+});*/
 
 module.exports = router;
