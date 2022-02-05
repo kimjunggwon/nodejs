4859cbd7f5cab2bf2a7bdb75256095359984232c
김중권 <gimjung-gwon@gimjung-gwon-ui-MacBookPro.local>
2022-02-05 18:46:51 +0900

    2022-02-05

--- node-auction/routes/index.js
+++ node-auction/routes/index.js
@@ -50,7 +50,7 @@ const upload = multer({
         },
         filename(req, file, cb){
             const ext = path.extname(file.originalname);
-            cb(null, path.basename(file.originalname, ext) + new Date().valueOf(). ext);
+            cb(null, path.basename(file.originalname, ext) + new Date().valueOf() + ext);
         },
     }),
     limits : { fileSize: 5 * 1024 * 1024 },
